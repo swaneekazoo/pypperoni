253e676bdb5a9bb1c5f4d3bc0298e6b52d0445b3
Adam Weir <adamintain@outlook.com>
2022-08-08 22:40:59 +0100

    22:40

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import xmltodict
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return 'index'
+
+
+@app.route('/scores')
+def scores():  # put application's code here
+    f = open('data/cases.xml', mode='rb')
+    o = xmltodict.parse(f.read())
+    f.close()
+    return o
 
 
 if __name__ == '__main__':
